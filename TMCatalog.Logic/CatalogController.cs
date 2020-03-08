775fbfd68010c05cc8eda7b93871b0781c1fa1fa
Patka Zsolt-Andras <andras.patka@gmail.com>
2020-03-08 18:20:49 +0200

    Added project structure provided by dvse

---
+++ TMCatalog.Logic/CatalogController.cs
@@ -0,0 +1,20 @@
+﻿// ------------------------------------------------------------------------------------------------------------------
+// <copyright file="CatalogController.cs" company="DVSE GmbH">
+//   Copyright (c) by DVSE Gesellschaft für Datenverarbeitung, Service und Entwicklung mbH. All rights reserved.
+// </copyright>
+// <author>Szilveszter Gorgicze</author>
+// ------------------------------------------------------------------------------------------------------------------
+
+namespace TMCatalog.Logic
+{
+    using TMCatalog.Model.DBContext;
+    public class CatalogController
+    {
+        private TMCatalogDB catalogDatabase;
+
+        public CatalogController()
+        {
+            this.catalogDatabase = new TMCatalogDB();
+        }
+    }
+}
---
+++ TMCatalog.ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿// ------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindowViewModel.cs" company="DVSE GmbH">
+//   Copyright (c) by DVSE Gesellschaft für Datenverarbeitung, Service und Entwicklung mbH. All rights reserved.
+// </copyright>
+// <author>Szilveszter Gorgicze</author>
+// ------------------------------------------------------------------------------------------------------------------
+
+namespace TMCatalog.ViewModel
+{
+  using TMCatalog.Common.MVVM;
+  using TMCatalog.Logic;
+
+  public class MainWindowViewModel : ViewModelBase
+   {
+
+    public static CatalogController CatalogController;
+
+    public MainWindowViewModel()
+    {
+      CatalogController = new CatalogController();
+      this.CloseCommand = new RelayCommand(this.CloseCommandExecute);
+    }
+
+    public RelayCommand CloseCommand { get; set; }
+
+    public void CloseCommandExecute()
+    {
+      ViewService.CloseDialog(this);
+    }
+  }
+}
